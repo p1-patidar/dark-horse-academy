c12f182b7e7e6bcb3225cb6154f6c14a0fde30d7
Pawan Patidar <pp@Pawans-MacBook-Pro.local>
2026-02-16 12:00:40 +0530

    Rebrand: Dark Horse Academy & PWA Implementation

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'dark-horse-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './privacy.html',
+  './manifest.json',
+  './assets/logo.png',
+  './assets/hero_background.png',
+  './assets/authority_figure.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://unpkg.com/aos@2.3.1/dist/aos.css',
+  'https://unpkg.com/aos@2.3.1/dist/aos.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
